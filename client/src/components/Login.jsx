9665d19d5ec749407f7d498cab4d8b285a9e705f
Joel Kong <joelkong2001@gmail.com>
2022-12-04 15:00:11 +0800

    feat: created form component in login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export const Login = () => {
-  return <div>hi</div>;
+  return (
+    <main className="background">
+      <section className="center">
+        <div className="login-form">
+          <div className="login-form-header">Log In</div>
+          <form className="login-form-input">
+            <input
+              className="login-form-key"
+              placeholder="Phone number, username or email"
+            ></input>
+            <input
+              className="login-form-password"
+              placeholder="Password"
+            ></input>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 };
