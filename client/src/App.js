10e793297915ea323b8c9fcd0a67bda78dce3ec1
Joel Kong <joelkong2001@gmail.com>
2022-12-04 13:33:16 +0800

    Init Login.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { Login } from "./components/Login.jsx";
 
 function App() {
+  const ProtectedRoute = ({ children, user }) => {
+    if (!user) {
+      return <Navigate to="/" />;
+    }
+    return children;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
   );
 }
 
