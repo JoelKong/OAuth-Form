3a13edc8d3513b701f9e5449686e276a732daa51
Joel Kong <joelkong2001@gmail.com>
2022-12-16 12:29:25 +0800

    chore: fixed profile picture

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -46,8 +46,9 @@ export const Home = () => {
         <div className="home-profile-picture">
           <img
             src={
-              user.profilePicture ||
-              "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+              user.profilePicture === undefined
+                ? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+                : user.profilePicture
             }
             alt="profilepicture"
             className="home-profile-picture__img"
