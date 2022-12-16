38edb9fd7d98e3eb10339b52815b8a3eb636c310
Joel Kong <joelkong2001@gmail.com>
2022-12-16 21:20:43 +0800

    feat: added reset password page with id and token as params

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Navigate } from "react-router-dom";
 import { Login } from "./components/Login/Login.jsx";
 import { Home } from "./components/Home/Home.jsx";
 import { Forgetpassword } from "./components/Login/Forgetpassword.jsx";
+import { Resetpassword } from "./components/Login/Resetpassword.jsx";
 
 let account = localStorage.getItem("email");
 
@@ -20,6 +21,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="forgetpassword" element={<Forgetpassword />} />
+        <Route path="reset-password/:id/:token" element={<Resetpassword />} />
         <Route
           path="home"
           element={
---
+++ client/src/components/Login/Resetpassword.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Resetpassword = () => {
+  return <div>sdf</div>;
+};
